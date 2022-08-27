d0d4adca0c60ad0fae1038d41ccc3a0da62c3eef
p2034 <kandakovdr.theend@gmail.com>
2022-08-27 22:37:44 +0300

    specifications

--- internal/requests/specifications.go
+++ internal/requests/specifications.go
@@ -13,5 +13,29 @@ func Specifications(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	SetResponse(w, settings.Conf, http.StatusOK)
+	SetResponse(w, response_specifications{
+		DebugMode:    settings.Conf.DebugMode,
+		Verification: settings.Conf.Verification,
+		Regex:        settings.Conf.Regex,
+		InviteCode:   settings.Conf.Security.InviteCode,
+	}, http.StatusOK)
+}
+
+type response_specifications struct {
+	DebugMode    bool `json:"debug-mode"`
+	Verification struct {
+		PasswordChange bool `json:"password-change"`
+		LoginChange    bool `json:"login-change"`
+		UserCreate     bool `json:"user-create"`
+		UserDelete     bool `json:"user-delete"`
+		TokenGet       bool `json:"token-get"`
+	} `json:"verification"`
+	Regex struct {
+		VerificationCode string `json:"verification-code"`
+		InviteCode       string `json:"invite-code"`
+		Login            string `json:"login"`
+		Password         string `json:"password"`
+		Token            string `json:"token"`
+	} `json:"regex"`
+	InviteCode bool `json:"invite-code"`
 }
